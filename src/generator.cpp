cc8088ba05a920438f7a78e2594f9a9d1f9ce8a4
Vinícius Campos <vinihcampos@B429-14044713L.imd.local>
2017-03-07 17:56:54 -0300

    create visualization with Treant.js

--- src/generator.cpp
+++ src/generator.cpp
@@ -19,7 +19,7 @@ bool is_number(const std::string & s){
 
 Json::Value populate_json(string pid){
 	Json::Value name;
-	name["name"] = Json::Value(pid);
+	name["name"] = Json::Value(pid + " " + processes[pid].first);
 
 	Json::Value children;
 	if(!processes[pid].second.empty()){
@@ -49,12 +49,15 @@ int main(int argn, char ** argc){
 
     directory_iterator end_itr;
 
+    queue<string> directory_has_subprocesses;
+
     for (directory_iterator itr(p); itr != end_itr; ++itr) {
 
     	if( is_directory( itr->path() ) && is_number(itr->path().leaf().string()) ){
-
     		ifstream file(itr->path().string() + "/stat", ifstream::in);
     		std::vector<string> parsed(std::istream_iterator<string>(file), {});
+
+            cout <<"Init: " << itr->path().string() << "/stat" << endl;
 
     		if( processes.find(  parsed[0] )  == processes.end()){
     			processes.insert( make_pair( parsed[0] , make_pair( parsed[1], vector<string>() ) ) );
@@ -68,31 +71,70 @@ int main(int argn, char ** argc){
 	    		}
 	    		processes[ parsed[3] ].second.push_back( parsed[0] );
     		}
-    		
+
+            if(exists(itr->path().string() + "/task")){
+                directory_has_subprocesses.push(itr->path().string() + "/task");
+            }    		
     	}
     }
 
-    /*for(auto it = processes.begin(); it != processes.end(); it++){
-    	cout << "PROCESS " << it->second.first << " | PID (" << it->first << ")\n";
-    	for(int i = 0; i < it->second.second.size(); ++i){
-    		cout << it->second.second[i] << " ";
-    	}
-    	cout << endl;
-	}*/
+    while(!directory_has_subprocesses.empty()){
+        string front = directory_has_subprocesses.front();
+        directory_has_subprocesses.pop();
+
+        cout << front << endl;
+
+        ifstream file(front + "/stat", ifstream::in);
+        std::vector<string> parsed(std::istream_iterator<string>(file), {});
+
+        cout << "File: " << front << "/stat\n"; 
+        if(!parsed.empty()){
+            if( processes.find(  parsed[0] )  == processes.end()){
+                processes.insert( make_pair( parsed[0] , make_pair( parsed[1], vector<string>() ) ) );
+            }else{
+                processes[  parsed[0] ].first = parsed[1];
+            }
+
+            if(parsed[3].compare("0")){
+                if(processes.find(  parsed[3] )  == processes.end()){
+                    processes.insert( make_pair( parsed[3] , make_pair( "", vector<string>() ) ) );
+                }
+                processes[ parsed[3] ].second.push_back( parsed[0] );
+            }
+
+            if(exists(front + "/task")){            
+                cout << "Queue: " << front << "/task\n";
+                directory_has_subprocesses.push(front + "/task");
+            }
+        }        
+    }
 
     Json::Value tree;
     Json::Value chartValue;
-    
+    Json::Value connectors;
+    Json::Value style;
+
+    connectors["type"] = Json::Value("step");
+    //style["stroke-width"] = Json::Value("2");
+    //style["stroke"] = Json::Value("#ccc");
+    //connectors["style"] = style;
+
     chartValue["container"] = Json::Value("#tree-simple");
+    chartValue["rootOrientation"] = Json::Value("WEST");
+    chartValue["nodeAlign"] = Json::Value("BOTTOM");
+    chartValue["connectors"] = connectors;
 
     tree["chart"] = chartValue;
+    
     tree["nodeStructure"] = populate_json(to_string(pid));
 
     //-------- Writing JSON ------------//
     Json::StyledWriter writer;
 
-    std::ofstream ofl("json/tree.json");
+    std::ofstream ofl("page/json/tree.json");
+    ofl << "var simple_chart_config = ";
     ofl << writer.write(tree);
+    ofl << ";";
     ofl.close();
 
 	return 0;
